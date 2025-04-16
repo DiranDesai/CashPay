3472858178d2f65b4c719b0f7a044601be40a577
DiranDesai <dirantechie@gmail.com>
2025-04-16 01:31:31 -0700

    Update codebase

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import logo from '../assets/logo.png'
+import google from '../assets/google.png'
+import twitter from '../assets/twitter.png'
+import phone from '../assets/phone.png'
+import { Link } from 'react-router-dom'
 
 function Login() {
   return (
     <div className="px-4 py-2">
         <div className="flex flex-col justify-center items-center">
-            <img src={logo} alt="" className="" />
+            <img src={logo} alt="" />
         </div>
         <form>
             <div className="mt-4">
@@ -16,7 +20,21 @@ function Login() {
                 <label htmlFor="password" className="font-medium">Password</label>
                 <input type="password" className="w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent" />
             </div>
+            <div className="flex justify-end mt-2">
+                <span className="text-sm font-medium"><Link>forgot password</Link></span>
+            </div>
             <button type="button" className="bg-red-700 w-full mt-10 text-white">Login</button>
+            <div className="flex justify-center items-center mt-4 space-x-7 mt-5">
+                <div className="bg-white p-5 shadow-md w-15 rounded-full h-15">
+                    <img src={google} alt="" />
+                </div>
+                <div className="bg-white p-5 shadow-md w-15 rounded-full h-15">
+                <img src={phone} alt="" />
+                </div>
+                <div className="bg-white p-5 shadow-md w-15 rounded-full h-15">
+                <img src={twitter} alt="" />
+                </div>
+            </div>
         </form>
     </div>
   )

32de2ee907f45d0d546a48b55d336b87b8105d54
DiranDesai <dirantechie@gmail.com>
2025-04-16 01:58:38 -0700

    Update codebase

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -15,14 +15,14 @@ function Login() {
         <form>
             <div className="mt-2">
                 <label htmlFor="email" className="font-medium">Email</label>
-                <input type="text" className="w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent" />
+                <input type="text" className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500 transition" />
             </div>
             <div className="mt-5">
                 <label htmlFor="password" className="font-medium">Password</label>
-                <input type="password" className="w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent" />
+                <input type="password" className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500 transition" />
             </div>
             <div className="flex justify-end mt-2">
-                <span className="text-sm font-medium"><Link>forgot password</Link></span>
+                <span className="text-sm font-medium underline"><Link>forgot password</Link></span>
             </div>
             <button type="button" className="w-full mt-10 text-white">Login</button>
             <div className="flex justify-center items-center mt-4 space-x-7 mt-10">
@@ -38,7 +38,7 @@ function Login() {
             </div>
             <div className="flex justify-center items-center mt-4 space-x-2 font-sm">
                 <p className="text-sm">Don't have an account?</p>
-                <Link to="/signup" className="text-blue-700">Sign Up</Link>
+                <Link to="/signup" className="text-blue-700">sign up</Link>
             </div>
         </form>
     </div>
